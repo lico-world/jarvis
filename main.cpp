55dbb1fe82a596f8b79ae355349fcf234d735655
lico-world <kilian.vincent@outlook.com>
2025-05-25 14:42:15 +0200

    VSCode configs to build/run

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 
 int main(int argc, char ** argv)
 {
-    std::cout<<"Lico world!"<<std::endl;
+    PaError err = Pa_Initialize();
+    if (err != paNoError)
+    {
+        std::cerr << "PortAudio error: " << Pa_GetErrorText(err) << std::endl;
+        return 1;
+    }
+
+    std::cout << "PortAudio initialized successfully!" << std::endl;
+    Pa_Terminate();
     return 0;
 }
